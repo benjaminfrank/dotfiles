dcd75927b0bc3b99d106fd6990b9fbf737d6b731
Samuel Halliday <samuel.halliday@trafigura.com>
2014-03-04 11:26:38 +0000

    screensaver

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,18 +2,21 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ICCCMFocus
 import XMonad.Hooks.SetWMName
+import XMonad.Util.EZConfig
 
 myNormalBorderColor  = "#7a7a7a"
 myFocusedBorderColor = "#000000"
 
 -- don't forget to follow http://youtrack.jetbrains.com/issue/IDEA-101072
 
-main = do
-  xmonad =<< xmobar defaultConfig
-            {
+main = xmonad $ defaultConfig {
 	      terminal = "urxvt",
               startupHook = setWMName "LG3D",
               logHook = takeTopFocus,
               normalBorderColor  = myNormalBorderColor,
               focusedBorderColor = myFocusedBorderColor
-	    }
+	    } `additionalKeys` [ 
+          ((mod1Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
