5605aabd03e7af7f9fddd3e06d9d8426f2d77321
Sam Halliday <sam.halliday@gmail.com>
2015-07-18 19:49:49 +0100

    desktop optimisations

--- Profiles/Samskara/.emacs.d/local.el
+++ Profiles/Samskara/.emacs.d/local.el
@@ -1,1 +1,5 @@
 (required 'darcula-theme (lambda() (set-frame-font "Inconsolata-16")) t)
+
+;; I'll usually want access to these..
+(find-file "~/Projects/ensime-emacs")
+(find-file "~/Projects/ensime-server")
