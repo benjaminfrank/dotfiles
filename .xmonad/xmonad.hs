a46e8339e4775122c39a36c6cc9ad42accdf92cb
Sam Halliday <sam.halliday@gmail.com>
2015-07-20 21:03:47 +0100

    more sensitive backlight control

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -54,8 +54,8 @@ keys' conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
     , ((modMask,               xK_t     ), withFocused $ windows . W.sink) -- %! Push window back into tiling
     , ((modMask .|. shiftMask, xK_q     ), io (exitWith ExitSuccess)) -- %! Quit xmonad
     , ((modMask .|. shiftMask, xK_r     ), spawn "xmonad --recompile && xmonad --restart") -- %! Restart xmonad
-    , ((0,                    0x1008ff02), spawn "xbacklight -inc 10")
-    , ((0,                    0x1008ff03), spawn "xbacklight -dec 10")
+    , ((0,                    0x1008ff02), spawn "xbacklight -inc 1")
+    , ((0,                    0x1008ff03), spawn "xbacklight -dec 1")
   ]
     ++
     -- workspaces
