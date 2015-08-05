98b063429d2fc26d6f80051ae4787aed4861c7f5
Sam Halliday <sam.halliday@gmail.com>
2015-08-05 23:02:51 +0100

    better light theme in emacs

--- Profiles/Sampo/.emacs.d/local.el
+++ Profiles/Sampo/.emacs.d/local.el
@@ -6,15 +6,7 @@
 ;;; Code:
 
 (required 'darcula-theme (lambda() (set-frame-font "Inconsolata-18")) t)
-
-(defun light-theme()
-  (interactive)
-  (required 'solarized (lambda()
-                         (setq
-                          solarized-high-contrast-mode-line t
-                          solarized-emphasize-indicators nil)
-                         (load-theme 'solarized-light)
-                         (set-frame-font "Inconsolata-20"))) t)
+;; REMINDER: to change theme `M-x load-theme RET TAB' (leuven is nice)
 
 ;; I'll usually want access to these..
 (find-file "~/Projects/")
