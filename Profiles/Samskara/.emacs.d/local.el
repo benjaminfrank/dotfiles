cd2cc457133be4cb8977fb95fc5a42284c82d90a
Sam Halliday <sam.halliday@gmail.com>
2016-05-15 11:13:04 +0100

    Hack font on desktop

--- Profiles/Samskara/.emacs.d/local.el
+++ Profiles/Samskara/.emacs.d/local.el
@@ -5,9 +5,10 @@
 ;;
 ;;; Code:
 
-(use-package darcula-theme
-  :config
-  (set-frame-font "Inconsolata-16"))
+(add-to-list 'default-frame-alist
+             '(font . "Hack-14"))
+
+(use-package darcula-theme)
 
 ;; I'll usually want access to these..
 (find-file (expand-file-name "scratch.el" user-emacs-directory))
